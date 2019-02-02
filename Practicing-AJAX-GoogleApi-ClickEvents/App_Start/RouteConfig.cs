5c71bd2f48791a1c93826249c044a92e07235e52
ArslanAmeer <arslan.ameer@hotmail.com>
2019-02-02 16:03:20 +0500

    ✨👍 Add: Google Controller / Views (Scaffolding)

--- Practicing-AJAX-GoogleApi-ClickEvents/App_Start/RouteConfig.cs
+++ Practicing-AJAX-GoogleApi-ClickEvents/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Practicing_AJAX_GoogleApi_ClickEvents
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Google", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Practicing-AJAX-GoogleApi-ClickEvents/Controllers/GoogleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practicing_AJAX_GoogleApi_ClickEvents.Controllers
+{
+    public class GoogleController : Controller
+    {
+        // GET: Google
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
