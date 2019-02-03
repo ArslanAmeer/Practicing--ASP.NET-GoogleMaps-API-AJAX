1be5f775e40500b5522945781a30b4f05279fea7
ArslanAmeer <arslan.ameer@hotmail.com>
2019-02-03 21:55:49 +0500

    🍱👍 Add: Google Maps Cords Model Class

---
+++ Practicing-AJAX-GoogleApi-ClickEvents/Models/GoogleMapCord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practicing_AJAX_GoogleApi_ClickEvents.Models
+{
+    public class GoogleMapCord
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+    }
+}
