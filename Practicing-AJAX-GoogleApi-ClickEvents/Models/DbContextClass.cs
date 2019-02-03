51d2e20eaf404cc23c569734e5bee9d8eb641d53
ArslanAmeer <arslan.ameer@hotmail.com>
2019-02-03 21:56:33 +0500

    🗃✨👍 Add: Setting Up Database Working Connection

---
+++ Practicing-AJAX-GoogleApi-ClickEvents/Models/DbContextClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Practicing_AJAX_GoogleApi_ClickEvents.Models
+{
+    public class DbContextClass : DbContext
+    {
+        public DbContextClass() : base("name=ConStr")
+        {
+
+        }
+
+        public DbSet<GoogleMapCord> GoogleMapCords { get; set; }
+
+
+    }
+}
