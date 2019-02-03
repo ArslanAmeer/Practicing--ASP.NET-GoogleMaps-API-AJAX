80037927d54d34e3a0f0930be01b8e152ee8bcab
ArslanAmeer <arslan.ameer@hotmail.com>
2019-02-03 22:54:20 +0500

    ✨🗃🗺 Add: Loading Cords to List From Database

--- Practicing-AJAX-GoogleApi-ClickEvents/Controllers/GoogleController.cs
+++ Practicing-AJAX-GoogleApi-ClickEvents/Controllers/GoogleController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Practicing_AJAX_GoogleApi_ClickEvents.Models;
 
 namespace Practicing_AJAX_GoogleApi_ClickEvents.Controllers
 {
     public class GoogleController : Controller
     {
+        private readonly DbContextClass db = new DbContextClass();
         // GET: Google
         public ActionResult Index()
         {
             return View();
         }
+
+        public JsonResult GetAllCords()
+        {
+            List<GoogleMapCord> cords;
+            using (db)
+            {
+                cords = (from s in db.GoogleMapCords select s).ToList();
+            }
+
+            return Json(cords, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
